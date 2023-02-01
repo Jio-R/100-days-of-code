7f835976eaedc23cec2ad89850d748e4ce7cb033
Jio <jioooo@pm.me>
2023-02-01 16:07:13 -0500

    Update day 1 main.py (overlord said to)

--- day-1/hello world/main.py
+++ day-1/hello world/main.py
@@ -1,1 +1,3 @@
-print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
+print("""Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')""")
