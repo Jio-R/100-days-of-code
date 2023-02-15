c1d3818a540653d1a5061974dc834733edb749bc
Jio <jioooo@pm.me>
2023-02-15 14:22:47 -0500

    Add comedy

--- day-2/day-2-exercises/day2-exercise-3-lifeInWeeks.py
+++ day-2/day-2-exercises/day2-exercise-3-lifeInWeeks.py
@@ -4,5 +4,9 @@ age = input("What is your current age? ")
 
 #Write your code below this line 👇
 
+while int(age) >= 90:
+    print("Sure you are.")
+    age = input("What is your current age? ")
+
 yearsLeft = 90-int(age)
 print("You have %s days, %s weeks, and %s months left." % (365*yearsLeft, 52*yearsLeft, 12*yearsLeft))
