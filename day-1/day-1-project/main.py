44b66db879297f7c308c7dfaaa5070b459d96568
Jio <jioooo@pm.me>
2023-02-15 13:09:22 -0500

    First day project final commit!

--- day-1/day-1-project/main.py
+++ day-1/day-1-project/main.py
@@ -1,11 +1,11 @@
 #1. Create a greeting for your program.
-print("Hello! Today you will choose a name for your (terrible) band based on the city you grew up in and the name of your favorite pet. Let's begin.\n")
+print("Hello! Today I will help you choose a name for your band based on the city you grew up in and the name of your pet. Let's begin.")
 #2. Ask the user for the city that they grew up in.
 city = input('What city did you grow up in?\n')
 #3. Ask the user for the name of a pet.
 pet = input("What is the name of your favorite pet?\n")
 #4. Combine the name of their city and pet and show them their band name.
-print('Your new band name is "The', city, pet + 's". What do you think?')
+print('Your new band name could be "The %s %ss". What do you think?' % (city, pet))
 #5. Make sure the input cursor shows on a new line:
-#no
+#done
 # Solution: https://replit.com/@appbrewery/band-name-generator-end
