4683bfd4776897ab4d1982ad84623736d877b4d8
Jio <jioooo@pm.me>
2023-02-15 15:08:50 -0500

    Add rounding to d2p

--- day-2/day-2-project/main.py
+++ day-2/day-2-project/main.py
@@ -11,4 +11,4 @@ bill = float(input("What's the bill? $"))
 tip = float(input("And how much will you be tipping today?\n(%): "))/100 + 1 #Tip saved as a multiplier, i.e. 1.2 for 20%
 people = int(input("Lastly, how many people will split the bill?\n"))
 
-print("Each person should pay: $%s" % (bill*tip/people))
+print("Each person should pay: $%s" % (round(bill*tip/people, 2)))
