d78e5043afbe4ccf78877081d8f7a2364d9c2735
Jio <jioooo@pm.me>
2023-02-15 14:19:21 -0500

    Add d2e3 solution

---
+++ day-2/day-2-exercises/day2-exercise-3-lifeInWeeks.py
@@ -0,0 +1,8 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+yearsLeft = 90-int(age)
+print("You have %s days, %s weeks, and %s months left." % (365*yearsLeft, 52*yearsLeft, 12*yearsLeft))
